b2d4d23fad9d56e4929fce89fc1a409e3939650f
Kexiang Wang <37156707+shawnwang66@users.noreply.github.com>
2020-12-13 19:47:23 -0600

    Add request install dependency

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ setup(name='colormap_dev',
       description='User defined colormaps in colormaps.dev',
       packages=['colormap_dev'],
       author_email='kwang66@illinos.edu',
+      install_requires=[
+            'request',
+      ],
       zip_safe=False)
